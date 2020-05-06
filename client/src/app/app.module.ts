083cd1a57fbc15213f0a0c88e380ef21cfcf56d7
Tshepo Moganedi <tshepo.moganedi@multichoice.co.za>
2020-05-06 21:33:25 +0200

    End of Section 8

---
+++ client/src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import {HttpClientModule} from '@angular/common/http';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavBarComponent
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
